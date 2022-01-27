c7ba38c595d9f7072dfefece8ba4b1d8df6388b4
solme <purplebora3@gmail.com>
2022-01-27 17:27:24 +0900

    up

---
+++ script/mcard_setting.js
@@ -0,0 +1,15 @@
+$(function(){
+
+	var swiper_1 = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 'auto',
+        paginationClickable: true,
+        spaceBetween: 0,
+        freeMode: true,
+        pagination: false
+    });
+
+
+
+
+});
